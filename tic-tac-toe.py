04cfc6aa69dee406df955747b7a6972a29a05a68
Yaren Kose <ksyarenn@gmail.com>
2024-07-12 16:17:06 +0300

    restart button added board on the center

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -4,7 +4,8 @@ import tkinter #* tk-interface (graphical user interface library)
 def set_tile(row,column):
     pass
 
-
+def new_game():
+    pass
 
 #* game setup
 
@@ -37,8 +38,25 @@ for row in range(3):
 
         board[row][column].grid(row= row+1, column= column)
 
+
+button = tkinter.Button(frame, text="Resart", font=("Consolas", 20),
+                        background=color_turquoise, foreground="white", command=new_game)
+
+button.grid(row=4, column=0, columnspan=3, sticky="we")
+
+
 frame.pack()
 
+
+#* center the window
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
 
 window.mainloop()
 
