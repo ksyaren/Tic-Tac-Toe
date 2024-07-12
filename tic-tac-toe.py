5563ebf8e8288770475b6ff0d1a31d184ec35ae5
Yaren Kose <ksyarenn@gmail.com>
2024-07-12 13:56:41 +0300

    window setup

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,6 +1,7 @@
 
 import tkinter #* tk-interface (graphical user interface library)
 
+#* game setup
 
 playerX = "X"
 playerO = "O"
@@ -13,3 +14,11 @@ color_yellow ="fac608"
 color_gray ="343434"
 color_light_turquoise ="14bdac"
 color_turquoise ="#14ae9f"
+
+#* window setup 
+window = tkinter.TK()
+window.title("Tic-Tac-toe")
+window.resizable(False, False) #* user cant expand the window
+
+window.mainloop()
+
