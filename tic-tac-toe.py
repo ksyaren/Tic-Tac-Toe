03d00ecdff3e47681131c4ebe1ffa27c150ebbe0
Yaren Kose <ksyarenn@gmail.com>
2024-07-12 13:46:27 +0300

    game setup

---
+++ tic-tac-toe.py
@@ -0,0 +1,15 @@
+
+import tkinter #* tk-interface (graphical user interface library)
+
+
+playerX = "X"
+playerO = "O"
+current_player = playerX
+board = [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]
+
+color_yellow ="fac608"
+color_gray ="343434"
+color_light_turquoise ="14bdac"
+color_turquoise ="#14ae9f"
